3dd6f4d68ebda6b239fb4aa5a1e877ac4a026d47
funhappyit <fun.happyit@gmail.com>
2021-02-22 17:54:50 +0900

    얕은 복사 , 깊은 복사, 프로토 타입 커밋

--- test.js
+++ test.js
@@ -1,19 +1,12 @@
-// 콜백 처리
-function logName() {
-  // 아래의 user 변수는 위의 코드와 비교하기 위해 일부러 남겨놓았습니다.
-  var user = fetchUser("domain.com/users/1", function (user) {
-    if (user.id === 1) {
-      console.log(user.name);
-    }
-  });
-}
-// async & await 적용 후
-async function logName() {
-  var user = await fetchUser("domain.com/users/1");
-  if (user.id === 1) {
-    console.log(user.name);
-  }
-}
-async function fetchData() {
-  await getUserList(); // Promise객체를 반환하는 API호출 함수 앞에 await
-}
+(function () {
+  var superObj = { superVal: "super" };
+  // Object.create(superObj)   // superObj를 부모로 하는 새로운 객체 생성
+  var subObj = Object.create(superObj); // 새로 생성한 객체를 subObj선언 및 할당
+  subObj.subVal = "sub"; // subObj의 속성으로 subVal 추가하고 값을 'sub'로 설정
+  console.log("subObj.subVal =>", subObj.subVal); // sub
+  console.log("subObj.superVal =>", subObj.superVal); // super
+  subObj.superVal = "sub"; // 객체의 프로퍼티가 없으면 추가하고, 있으면 갱신해 준다.
+  //그 객체의 원형을 바꾸는 것이 아니다.
+  //그 객체의 원형을 바꾸려면, 그 객체의 원형을 가리키는 subObj.__proto__.superVal = 'sub' 와 같은 코드로 작성해야 한다. 하지만 이렇게 사용하지는 않는다.
+  console.log("superObj.superVal =>", superObj.superVal); // super
+})();
