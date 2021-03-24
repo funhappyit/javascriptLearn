9c055ee91dd509e2fd3c709e01a27792c6131d09
funhappyit <fun.happyit@gmail.com>
2021-03-24 12:43:49 +0900

    자스스톤 쫄병카드 코스트에 맞춰 뽑기 커밋

--- js2/자스스톤.js
+++ js2/자스스톤.js
@@ -6,22 +6,42 @@ var 상대덱data = [];
 var 내덱data = [];
 var 상대영웅data;
 var 내영웅data;
+//중복돼는 부분 합치기
+function 카드돔연결(데이터, 돔, 영웅) {
+  var 카드 = document.querySelector(".card-hidden .card").cloneNode(true);
+  카드.querySelector(".card-cost").textContent = 데이터.cost;
+  카드.querySelector(".card-att").textContent = 데이터.att;
+  카드.querySelector(".card-hp").textContent = 데이터.hp;
+  if (영웅) {
+    카드.querySelector(".card-cost").style.display = "none";
+    var 이름 = document.createElement("div");
+  }
+  돔.appendChild(카드);
+}
+
 function 상대덱생성(개수) {
   for (var i = 0; i < 개수; i++) {
     상대덱data.push(카드공장());
   }
+  상대덱data.forEach(function (data) {
+    카드돔연결(data, 상대덱);
+  });
 }
 function 내덱생성(개수) {
   for (var i = 0; i < 개수; i++) {
     내덱data.push(카드공장());
   }
-  console.log("내덱data" + 내덱data[0].att + 내덱data[0].hp + 내덱data[0].cost);
+  내덱data.forEach(function (data) {
+    카드돔연결(data, 내덱);
+  });
 }
 function 내영웅생성() {
-  카드공장();
+  내영웅data = 카드공장(true);
+  카드돔연결(내영웅data, 내영웅, true);
 }
 function 상대영웅생성() {
-  카드공장();
+  상대영웅data = 카드공장(true);
+  카드돔연결(상대영웅data, 상대영웅, true);
 }
 function 초기셋팅() {
   상대덱생성(5);
@@ -30,13 +50,19 @@ function 초기셋팅() {
   상대영웅생성();
 }
 
-function Card() {
-  this.att = Math.ceil(Math.random() * 5);
-  this.hp = Math.ceil(Math.random() * 5);
-  this.cost = Math.floor((this.att + this.hp) / 2);
+function Card(영웅) {
+  if (영웅) {
+    this.att = Math.ceil(Math.random() * 2);
+    this.hp = Math.ceil(Math.random() * 5) + 25;
+    this.hero = true;
+  } else {
+    this.att = Math.ceil(Math.random() * 5);
+    this.hp = Math.ceil(Math.random() * 5);
+    this.cost = Math.floor((this.att + this.hp) / 2);
+  }
 }
 // 조금씩 다른 객체들을 찍어내기 위해서 팩토리 패턴을 사용한다.
-function 카드공장() {
-  return new Card();
+function 카드공장(영웅) {
+  return new Card(영웅);
 }
 초기셋팅();
